26a985b82d267a4862083b0aa85439ed5c12d5ad
Lidiane Lili <gustavo_souza.dev@outlook.com>
2023-08-03 01:58:50 -0300

    adicionando funções: gerar data atual, configurando valores maximos e mininos nos inputs e validação dos inputs

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+function gerarDadaAtual() {
+	dataAtual = {
+		dia: 0,
+		mes: 0,
+		ano: 0
+	}
+	let dataHoraAtual = Intl.DateTimeFormat('pt-BR', 
+	{dateStyle: "short"}).format(Date.now())
+
+	dataAtual.dia = parseInt(dataHoraAtual.substring(0, 2));
+	dataAtual.mes = parseInt(dataHoraAtual.substring(3, 5));
+	dataAtual.ano = parseInt(dataHoraAtual.substring(6, 10));
+	return (dataAtual)
+}
+
+function inputMinMax(dataAtual) {
+	let input = ''
+
+	input = document.getElementById("ano-nascimento");
+	input.setAttribute("max", dataAtual.ano)
+	input.setAttribute("min", 1)
+
+	input = document.getElementById("mes-nascimento");
+	input.setAttribute("max", 12)
+	input.setAttribute("min", 1)
+
+	input = document.getElementById("dia-nascimento");
+	input.setAttribute("max", 31)
+	input.setAttribute("min", 1)
+}
+
+function validacaoData(usuario, dataAtual) {
+	if (usuario.ano == dataAtual.ano) {
+		if (usuario.mes == dataAtual.mes) {
+			if (usuario.dia > dataAtual.dia) {
+				console.log("A data informada não pode estar no futuro.");
+				return (-1);
+			}
+		}
+		if (usuario.mes > dataAtual.mes) {
+			console.log("A data informada não pode estar no futuro.");
+			return (-1);	
+		}
+	}
+}
 
 function calculadoraDeIdade(event) {
 	let dadosUsuario = {
@@ -8,15 +53,21 @@ function calculadoraDeIdade(event) {
 		idade: 0,
 		faixaEtaria: ''
 	}
-	event.preventDefault()
-	pegarValores(dadosUsuario);
+	event.preventDefault();
+	let dataAtual = gerarDadaAtual();
+	pegarValores(dadosUsuario, dataAtual);
+	if (validacaoData(dadosUsuario, dataAtual) == -1)
+		return (-1)
+	if (isNaN(dadosUsuario.dia) || isNaN(dadosUsuario.mes) || isNaN(dadosUsuario.ano))
+		return (-1)
 	dadosUsuario.idade = calcularIdade(dadosUsuario);
 	dadosUsuario.faixaEtaria = classificarFaixaEtaria(dadosUsuario.idade);
 	cadastrarUsuario(dadosUsuario)
-	console.log(dadosUsuario);
+	window.location.reload();
 }
 
-function pegarValores(dadosUsuario) {
+function pegarValores(dadosUsuario, dataAtual) {
+	inputMinMax(dataAtual)
 	dadosUsuario.nome = document.getElementById("nome").value.trim();
 	dadosUsuario.dia = parseInt(document.getElementById("dia-nascimento").value.trim());
 	if (isNaN(dadosUsuario.dia))
@@ -26,27 +77,22 @@ function pegarValores(dadosUsuario) {
 		console.log("O mês não foi informado corretamente.");
 	dadosUsuario.ano = parseInt(document.getElementById("ano-nascimento").value.trim());
 	if (isNaN(dadosUsuario.ano))
-		console.log("O ano não foi informado corretamente.");	
+		console.log("O ano não foi informado corretamente.");
 }
 
 function calcularIdade(dadosUsuario) {
-	let dataHoraAtual = Intl.DateTimeFormat('pt-BR', 
-	{dateStyle: "short"}).format(Date.now())
-
-	const anoAtual = parseInt(dataHoraAtual.substring(6, 10));
-	const mesAtual = parseInt(dataHoraAtual.substring(3, 5));
-	const diaAtual = parseInt(dataHoraAtual.substring(0, 2))
+	let dataAtual = gerarDadaAtual();
 
 	// console.log(anoAtual, mesAtual, diaAtual);
 	// const anoAtual = new Date().getFullYear();
 	// const mesAtual = new Date().getMonth();
 	// const diaAtual = new Date().getDay();
 
-	let idade = anoAtual - dadosUsuario.ano;
-	if (mesAtual < dadosUsuario.mes)
+	let idade = dataAtual.ano - dadosUsuario.ano;
+	if (dataAtual.mes < dadosUsuario.mes)
 		idade -= 1;
-	else if (mesAtual == dadosUsuario.mes){
-		if (diaAtual < dadosUsuario.dia)
+	else if (dataAtual.mes == dadosUsuario.mes){
+		if (dataAtual.dia < dadosUsuario.dia)
 			idade -= 1;
 	}
 	return (idade)
